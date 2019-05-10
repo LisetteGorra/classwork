93e5bb3705e8325d726fbfbb6916ead8baf69043
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-10 11:10:35 -0400

    convert string to an array with js code, used .split and indexOf to find specific item index

---
+++ name.js
@@ -0,0 +1,9 @@
+var string = "Stuart Yoshi Denton Hayley Indigo Timothy Brianna Nathaniel Ezekiel Cairo Tate Ruby Ria Laith Kieran Solomon Patricia Nicholas Carl Randall Nomlanga Nita Daria Courtney Minerva Burke Beatrice Ella Candice Brennan Garrett Lois Alfonso Camilla Luke Kirk Olga Ayanna Honorato Alexandra Ora Sigourney Ronan Bo Alma Oliver Hunter Kyle Maile Evelyn Kieran Farrah Carly Claude Arnell Austin Autumn Jessica George Deanna Shannon Beck Chancellor Christian Yeo Keegan Regina Kyle Herrod Harper Timon Amanda Craig Hu Noble Yoko Shay Octavia Kamal Linus Sierra Martena Callum Ivana Michelle Veda Yetta Rana Felix Inga Summer Allistair Martena Vincent Laith Kim Maria Ciara Desirae Inga Emily Janna Kasimir Tatiana Emily";
+
+var res = string.split(" ");
+
+console.log(res.length);
+
+var A = res.indexOf("Arnell");
+
+console.log(A);
