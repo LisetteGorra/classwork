c63a160af8dc474bb7c25449bab11a9bda0cacb6
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-12 11:06:34 -0400

    Used for loop and if statement to find name and position of name in array

--- hello10x.js
+++ hello10x.js
@@ -1,6 +1,10 @@
 var hello = " hello 10 times!";
+var names = ["Arnell", "David", "Lisette", "Waldo", "Joseph", "Stephanie", "Nicole"];
 
-for ( var i = 0; i <= 9; i++){
-    console.log(hello);
+for ( var i = 0; i <= names.length; i++){
+    if(names[i] == "Waldo"){
+        console.log("Found", names[i], "in position", i);
+    }
 }
+
 
