8295d83e69c70c72b4ff6b02d49e3409d8847c45
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-14 11:37:21 -0400

    Adjusted console to reflect correct position by adding 1 to i

--- arrayloops3.js
+++ arrayloops3.js
@@ -10,7 +10,7 @@ for (i = 0; i < arrayGuestList.length; i++ ) {
     if ( arrayGuestList[i] == "Nicole") {
         counter++;
         if (counter == 1) {
-            console.log("Found", arrayGuestList[i], "in position", i);
+            console.log("Found", arrayGuestList[i], "in position", i +1);
         }
     }
 }
