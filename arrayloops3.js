b4c3ad9371eaf264c176ec3ecb1248819a16a8e9
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-14 11:30:38 -0400

    Created forloop, that uses counter and if statement to return position and element in position

---
+++ arrayloops3.js
@@ -0,0 +1,16 @@
+var guestList = "Lisette Nicole Arnell Joseph Jason";
+
+var arrayGuestList = guestList.split(" ");
+
+var i;
+
+var counter = 0;
+
+for (i = 0; i < arrayGuestList.length; i++ ) {
+    if ( arrayGuestList[i] == "Nicole") {
+        counter++;
+        if (counter == 1) {
+            console.log("Found", arrayGuestList[i], "in position", i);
+        }
+    }
+}
--- hello10x.js
+++ hello10x.js
@@ -1,9 +1,14 @@
 var hello = " hello 10 times!";
-var names = ["Arnell", "David", "Lisette", "Waldo", "Joseph", "Stephanie", "Nicole"];
+var names = ["Arnell", "David", "Lisette", "Waldo", "Joseph", "Stephanie", "Nicole", "Waldo"];
+var counter = 0;
 
 for ( var i = 0; i <= names.length; i++){
     if(names[i] == "Waldo"){
+        counter++;
+        if(counter == 1){
         console.log("Found", names[i], "in position", i);
+        }
+        
     }
 }
 
