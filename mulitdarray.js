6ede6990b2e2428d8291bdd2f2e508932cf620ec
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-18 15:03:04 -0400

    Used for loop and splice to insert item into multi-d array

--- mulitdarray.js
+++ mulitdarray.js
@@ -4,7 +4,8 @@ var dpt_employeeId_info = ["10001, d001", "10002, d002", "10003, d003", "10004, 
 var employee_info = ["10001, Arnell Millhouse", "10002, Phen Thach", "10003, Alex Mendez", "10004, Troy Goode", "10005, Sylvester Nyeswa"];
 
 var departments = [];
-var tempSplitInfo = "";
+var employeeId = [];
+var tempSplitInfo = [];
 
 // var arraySplit_dpt_employeeId_info = dpt_employeeId_info.split("");
 // console.log(arraySplit_dpt_employeeId_info);
@@ -14,18 +15,27 @@ var tempSplitInfo = "";
 //Create a set of RELATIONAL arrays that hold employee data (employee ID, employee name and department id).
 //QUASI- CODE
 // Create an array withjust the department info with a forloop 
-
+// using push so we don't have to reapeat code // DRY = Don't Repeat Yourself
 for ( var i = 0; i < dpt_employeeId_info.length; i++) {
-    tempSplitInfo = dpt_employeeId_info[i].split(", "); // will return seperate arrays, between the commas between the quotes of info array
-    departments.push(tempSplitInfo[1]);
-    for ( var j = 0; j < departments.length; j++){
-        if ( departments[i] == departments[i]){ 
-        
-    }
-    
-        
-    }// the 1 reps the 1st part of split array
+    tempSplitInfo.push(dpt_employeeId_info[i].split(", ")); // will return seperate arrays, between the commas between the quotes of info array
+    departments.push(tempSplitInfo[i][1]); //the 1 reps the 1st part of split array
+}
+departments.sort();
 
+departments = [...new Set(departments)]; // de-dups the array, and creates new array with unique elements
+
+for ( i = 0; i < departments.length; i ++ ){ // add empty sub arrays to the empty employeeId array
+    employeeId.push([]);
+ 
+}
+console.log(employeeId); // this wil show the empty arrays
+for ( i = 0;  i < tempSplitInfo.length; i++) {
+    employeeId.splice(departments.indexOf("d001"), 0, tempSplitInfo[0][0] );
 }
 
-console.log(departments);
+// console.log(tempSplitInfo);
+// console.log(departments);
+console.log(employeeId); // this will show the filled arrays
+
+
+ 
