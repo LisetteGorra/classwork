23f7b707abc65a13781347470c04bae364694a74
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-31 11:23:25 -0400

    Demonstrated the difference between let, var and const

--- variables.js
+++ variables.js
@@ -5,3 +5,40 @@
 
 // create a program that demonstrates the difference between var,let and const
 
+var varDemo = "Lisette";
+console.log(varDemo);
+
+varDemo = "Gorra";
+console.log(varDemo);
+
+function names(){
+    varDemo = "Lisette Gorra";
+}
+names();
+
+const car = {type:"Fiat", model:"500", color:"white"};
+console.log(car);
+car.color = "grey";
+console.log(car);
+car.type = "Camry";
+console.log(car);
+car = "auto"; // Here we cannot change the value, because of const
+console.log(car);
+
+
+var auto = {type:"Fiat", model:"500", color:"white"};
+console.log(car);
+auto.color = "grey";
+console.log(car);
+auto.type = "Camry";
+console.log(car);
+auto = "whip"; // Here we are able to change the value using var
+console.log(auto);
+
+function convertToF(celsius) {
+  let fahrenheit;
+  fahrenheit = celsius * 9/5 + 32;
+  return fahrenheit;
+}
+
+convertToF(30);
