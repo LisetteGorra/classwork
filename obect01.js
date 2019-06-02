ff6bb1956b9926ed484f19fea8982e89432bccaa
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-02 11:48:12 -0400

    Corrected psuedo code logic in order to return string or number from array with if else statement

--- obect01.js
+++ obect01.js
@@ -6,4 +6,7 @@ var infoArray = ["info1", 12, "info2", 25, "info3"];
 
 // psuedo code for resolutions
 //for loop i = 1
-//if i % 2 = 0 then infoArray[i] is typeOf
+//if i % 2 = 0 || if 1 % 2 = 1, then console.log("Number here!")
+//else console.log("String or something else here!")
+
+var infoNumbers = [12, 25, 10];
