33e331140e7bfe969b62224fd08154a710d67745
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-02 11:38:15 -0400

    Creating program to understand how information is stored in arrays and variables

---
+++ obect01.js
@@ -0,0 +1,9 @@
+// storing information and data 
+
+var variables = "info";
+
+var infoArray = ["info1", 12, "info2", 25, "info3"];
+
+// psuedo code for resolutions
+//for loop i = 1
+//if i % 2 = 0 then infoArray[i] is typeOf
