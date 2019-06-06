ceb7ab4c6f04b72532588fbba5b9cd0350781e45
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-06 11:30:39 -0400

    Console logged for browser readability, and converted number into currency for totak bill plus tax program

--- objects/object03.js
+++ objects/object03.js
@@ -1,9 +1,11 @@
 var rentalCar = {
     totalDaysRented: 30,
-    rate: 50,
+    dailyRate: 50,
     carType: "Sedan",
     taxRate: .07
 };
 
-var bill = (rentalCar.totalDaysRented * rentalCar.rate) + ((rentalCar.rate * rentalCar.totalDaysRented) *rentalCar.taxRate);
-console.log(bill);
+var bill = (rentalCar.totalDaysRented * rentalCar.dailyRate) + ((rentalCar.dailyRate * rentalCar.totalDaysRented) * rentalCar.taxRate);
+console.log("Your total bill for your rental car is:", bill.toLocaleString('en-us', {style: 'currency', currency: 'USD'})+ "!" );
+
+// .toLocaleString('en-us', {style: 'currency', currency: 'USD'})+ "!" 
